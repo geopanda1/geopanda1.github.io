f7793d489e53a3717e380c02e088cc51f622bfdf
geopanda1 <andikollert@gmail.com>
2020-01-20 09:16:49 +0100

    first commit

---
+++ heatmap_marker.js
@@ -0,0 +1,9 @@
+window.heatmapMarker = {
+"type": "FeatureCollection",
+"name": "heatmap_marker",
+"features": [
+{ "type": "Feature", "properties": { "fid": 1, "filename": "Camera_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.406273205449299, 47.266397863272282 ] } },
+{ "type": "Feature", "properties": { "fid": 2, "filename": "Park_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.405142597998333, 47.266784507126793 ] } },
+{ "type": "Feature", "properties": { "fid": 3, "filename": "People_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.405531414009328, 47.26670250821946 ] } }
+]
+}
