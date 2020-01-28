117c9f58dacc685fd91612918a91eb9a973af93b
geopanda1 <andikollert@gmail.com>
2020-01-28 15:50:31 +0100

    first commit

---
+++ imot-heatmaps/heatmap_marker.js
@@ -0,0 +1,14 @@
+ window.heatmapMarker = 
+{
+"type": "FeatureCollection",
+"name": "heatmaps",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "filename": "Camera_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.406273205449299, 47.266397863272282 ] } },
+{ "type": "Feature", "properties": { "filename": "Park_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.405142597998333, 47.266784507126793 ] } },
+{ "type": "Feature", "properties": { "filename": "People_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.405531414009328, 47.26670250821946 ] } },
+{ "type": "Feature", "properties": { "filename": "Bridge_2.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.406466482827996, 47.265783311919833 ] } },
+{ "type": "Feature", "properties": { "filename": "Smartphone_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.406415701881331, 47.265478125904494 ] } },
+{ "type": "Feature", "properties": { "filename": "See_1.jpg" }, "geometry": { "type": "Point", "coordinates": [ 11.406037430258959, 47.26570993928916 ] } }
+]
+}
